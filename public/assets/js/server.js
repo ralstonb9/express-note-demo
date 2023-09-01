a0392d000e7ff6d0c02a39f6b9ae0ce3ec9fc307
ralstonb9 <ralstonb9@gmail.com>
2023-08-31 18:42:19 -0700

    added get requests

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -2,8 +2,18 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-api.use()
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, './db/db.json')).then((data) => res.json(JSON.parse(data)))
+})
