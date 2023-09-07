bb8ffe983d325351b6500fe79aec1dbddd5d02ac
ralstonb9 <ralstonb9@gmail.com>
2023-09-06 18:29:07 -0700

    added get and post methods to server.js

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -16,4 +16,26 @@ app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/note
 
 app.get('/api/notes', (req, res) => {
     fs.readFile(path.join(__dirname, './db/db.json')).then((data) => res.json(JSON.parse(data)))
-})
+});
+
+app.post('/api/notes', (req, res) => {
+    const {title, text} = req.body;
+   
+    if(title && text){
+        const newPost = {
+            title,
+            text,
+            id: uuid(),
+        };
+        
+        fs.readFile(path.join(__dirname, './db/db.json'), 'utf8', (err, data) => {
+            const parsedPosts = JSON.parse(data);
+            parsedPosts.push(newPost);
+        });
+    
+        fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(parsedPosts, null, 2), (err) => {
+            if (err) throw err;
+            res.json(newPost);
+        });
+    }
+});
